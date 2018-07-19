63bc09cd87528fd5d37c7bd41975205e929e9b43
daniel ariel <danielariel70@gmail.com>
2018-07-19 19:00:47 +0200

    setup and some css

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+// const hb = require("express-handlebars");
+// const fs = require("fs");
+
+// const { contentEn } = require("./webpageContent.js");
+
+// app.engine("handlebars", hb());
+// app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/assets"));
+app.use(express.static(__dirname + "/favicon"));
+// app.use(express.static(__dirname + "/index.html"));
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false
+//     })
+// );
+
+// let myProjects = fs.readdirSync(__dirname + "/projects");
+//
+// var myProjectObjects = myProjects.map(function(project) {
+//     return {
+//         dirName: project,
+//         heroku_url: require("./projects/" + project + "/project.json")
+//             .heroku_url,
+//         github_url: require("./projects/" + project + "/project.json")
+//             .github_url,
+//         createdWith: require("./projects/" + project + "/project.json")
+//             .createdWith,
+//         comment_link: require("./projects/" + project + "/project.json")
+//             .comment_link,
+//         comment_url: require("./projects/" + project + "/project.json")
+//             .comment_url,
+//         displayName: require("./projects/" + project + "/project.json")
+//             .displayName,
+//         description: require("./projects/" + project + "/project.json")
+//             .description,
+//         tryMsg: require("./projects/" + project + "/project.json").tryMsg,
+//         codeMsg: require("./projects/" + project + "/project.json").codeMsg,
+//         comment: require("./projects/" + project + "/project.json").comment
+//     };
+// });
+
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 8080, () =>
+    console.log(`port 8080 is listening`)
+);
---
+++ public/script.js
@@ -0,0 +1,12 @@
+// {
+//     "displayName": "PinApp",
+//     "description": "PinApp is a mobile friendly Web Application that allows users to easily add, customize and share their favorite locations.",
+//     "tryMsg":"Add pins at",
+//     "heroku_url":"https://pinapp-spiced.herokuapp.com",
+//     "codeMsg":"Check out the code",
+//     "github_url":"https://github.com/patrinoua/finalproject",
+//     "createdWith":"Made with Node.js, React, Redux and Socket.io",
+//     "comment": "Project developed in collaboration with",
+//     "comment_link": "Daniel Ariel",
+//     "comment_url": "https://github.com/danielarielS"
+// }
